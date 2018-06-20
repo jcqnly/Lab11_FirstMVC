ae93b85b3aeba424bb3fd0b2f44d61b9becbb65c
jcqnly <jcqnly@gmail.com>
2018-06-19 21:50:48 -0700

    add routing to index and then results view.

--- TimePersonOfTheYear/TimePersonOfTheYear/Controllers/HomeController.cs
+++ TimePersonOfTheYear/TimePersonOfTheYear/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TimePersonOfTheYear.Models;
+using TimePersonOfTheYear;
 
 
 namespace TimePersonOfTheYear.Controllers
@@ -11,22 +12,30 @@ namespace TimePersonOfTheYear.Controllers
     public class HomeController : Controller
     {
         //default action for the controller
+        [HttpGet]
         public IActionResult Index()
         {
-            //instantiate each person
-
-
             //Collections initializer
-            List<Person> timePerson = 
-                new List<Person> { };
+            //List<Person> timePerson = new List<Person>();
 
-            return View(timePerson);
+            //timePerson = Person.GetPersons(1990, 2000);
+            //return View(timePerson);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int beginYear, int endYear)
+        {
+            return RedirectToAction("Results", new { beginYear, endYear });
         }
 
         //redirect user to another page
-        public IActionResult Class()
+        public IActionResult Results(int beginYear, int endYear)
         {
-            return RedirectToAction();
+            ViewData["Message"] = "Person Of The Year";
+            Person person = new Person();
+
+            return View(Person.GetPersons(beginYear, endYear));
         }
     }
 }
