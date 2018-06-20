4ef3731f1e450d2698420d43ca10af6b7a97922d
jcqnly <jcqnly@gmail.com>
2018-06-19 22:31:43 -0700

    search functionality up

--- TimePersonOfTheYear/TimePersonOfTheYear/Controllers/HomeController.cs
+++ TimePersonOfTheYear/TimePersonOfTheYear/Controllers/HomeController.cs
@@ -15,27 +15,22 @@ namespace TimePersonOfTheYear.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            //Collections initializer
-            //List<Person> timePerson = new List<Person>();
-
-            //timePerson = Person.GetPersons(1990, 2000);
-            //return View(timePerson);
             return View();
         }
 
         [HttpPost]
         public IActionResult Index(int beginYear, int endYear)
         {
-            return RedirectToAction("Results", new { beginYear, endYear });
+            return RedirectToAction("Results", new { newBeginYear = beginYear, newEndYear = endYear });
         }
 
         //redirect user to another page
-        public IActionResult Results(int beginYear, int endYear)
+        public IActionResult Results(int newBeginYear, int newEndYear)
         {
             ViewData["Message"] = "Person Of The Year";
             Person person = new Person();
 
-            return View(Person.GetPersons(beginYear, endYear));
+            return View(Person.GetPersons(newBeginYear, newEndYear));
         }
     }
 }
